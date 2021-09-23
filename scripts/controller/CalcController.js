92adddccc392043c200dc9c334bf41af5f311cea
feh-franc0 <fernandofv1110@gmail.com>
2021-09-23 20:25:06 -0300

    Aplicando vários Eventos usando o split()

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,21 +1,20 @@
 class CalcController {
 
     constructor(){
-        //use o "_" na frente do atributo para ter o acesso privado e use get e set para os mesmos.
+
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
-        this.initButtonsEvents()
+        this.initButtonsEvents();
+
     }
 
-    initialize(){ //método principal do nosso projeto.
-        //setInterval : função executada em um intervalo de tempo, o tempo é marcado em milisegundos.
-        //setTimeout : executa algo apenas uma vez e depois do tempo definido.
+    initialize(){
 
-        this.setDisplayDateTime();
+        this.setDisplayDateTime()
 
         setInterval(()=>{
 
@@ -25,55 +24,95 @@ class CalcController {
 
     }
 
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        })
+    
+    }
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index)=>{
-            btn.addEventListener('click', e=>{
+
+            this.addEventListenerAll(btn, "click drag", e => {
+
                 console.log(btn.className.baseVal.replace("btn-",""));
+
             })
 
-        });
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+
+            })
+
+        })
 
     }
 
-    setDisplayDateTime(){//sempre que for reutilizar algum codigo,crie um metodo.
-            
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
-            day:"2-digit",
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
             month: "long",
             year: "numeric"
         });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
 
-    }    
+    }
 
     get displayTime(){
+
         return this._timeEl.innerHTML;
+
     }
+
     set displayTime(value){
+
         return this._timeEl.innerHTML = value;
+
     }
 
     get displayDate(){
+
         return this._dateEl.innerHTML;
-    }
-    set displayDate(value){
-        return this._dateEl.innerHTML = value;
+
     }
 
-    get displayCalc(){ //get é para saber o que vai ser dovolvido toda vez que chamarmos.
-        return this._displayCalcEl.innerHTML;
+    set displayDate(value){
+
+        return this._dateEl.innerHTML = value;
+
     }
-    set displayCalc(value){ //set é para salvarmos/mudarmos o valor do que desejarmos:
+
+    get displayCalc(){
+
+        return this._displayCalcEl.innerHTML;
+
+    }
+
+    set displayCalc(value){
+
         this._displayCalcEl.innerHTML = value;
+
     }
 
     get currentDate(){
+
         return new Date();
+
     }
+
     set currentDate(value){
+
         this._currentDate = value;
+
     }
+
 }
