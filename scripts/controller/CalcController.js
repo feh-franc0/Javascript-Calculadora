d8dfbe249ad302650c320c521375fecfd5ba0ee7
feh-franc0 <fernandofv1110@gmail.com>
2021-09-28 21:23:19 -0300

    Botão porcento.Todos os calculos funcionando,inclusive o igual e o porcento

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -23,6 +23,8 @@ class CalcController {
 
         }, 1000);
 
+        this.setLastNumberToDisplay();
+
     }
 
     addEventListenerAll(element, events, fn){
@@ -38,12 +40,16 @@ class CalcController {
     clearAll(){
 
         this._operation = [];
+        this.setLastNumberToDisplay();
+
 
     }
 
     clearEntry(){
 
         this._operation.pop();
+        this.setLastNumberToDisplay();
+
 
     }
 
@@ -76,12 +82,25 @@ class CalcController {
     }
 
     calc(){
-        
-        let last = this._operation.pop();
 
+        let last ='';
+
+        if (this._operation.length > 3){
+            last = this._operation.pop();
+        }
+        
         let result = eval(this._operation.join(""));
 
-        this._operation = [result, last];
+        if (last == '%') {
+            result /= 100;
+
+            this._operation = [result]
+
+        }else{    
+            this._operation = [result];   
+
+            if (last) this._operation.push(last);
+        }
 
         this.setLastNumberToDisplay();
 
@@ -99,6 +118,8 @@ class CalcController {
             }
 
         }
+
+        if (!lastNumber) lastNumber = 0;
 
         this.displayCalc = lastNumber;
 
@@ -182,7 +203,7 @@ class CalcController {
                 break;
 
             case 'igual':
-
+                this.calc();
                 break;
 
             case 'ponto':
